9817ec13e838fde6090bcb78d8af2399552cea38
Marcelo0920 <marcelo0920.z@gmail.com>
2023-11-07 18:18:17 -0400

    added user model and others models, added server file but didnt connect to DB in mongo

--- server/app.js
+++ server/app.js
@@ -1,3 +1,8 @@
+import { config } from "dotenv";
 import express from "express";
+
+config({
+  path: "./data/config.env",
+});
 
 export const app = express();
---
+++ server/models/user.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "El nombre es requerido"],
+  },
+  apellido: {
+    type: String,
+    required: [true, "El apellido es requerido"],
+  },
+  email: {
+    type: String,
+    unique: [true, "Este correo ya esta en uso"],
+    validate: validator.isEmail,
+  },
+  telefono: {
+    type: Number,
+    unique: [true, "Este numero ya esta en uso"],
+    validate: validator.isNumeric,
+  },
+  password: {
+    type: String,
+    required: [true, "La contrasenia es requerida"],
+    minLength: [6, "La contrasenia debe ser de al menos 6 caracteres"],
+  },
+  avatar: {
+    public_id: String,
+    url: String,
+  },
+});
