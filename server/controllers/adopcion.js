16296825dd7e5e2ef4519ba0edc0772da134e54a
Marcelo0920 <marcelo0920.z@gmail.com>
2023-11-09 10:07:26 -0400

    added error handler for registrarAdopcion

--- server/controllers/adopcion.js
+++ server/controllers/adopcion.js
@@ -9,25 +9,29 @@ export const registrarAdopcion = async (req, res) => {
     return res.status(400).json({ errors: errors.array() });
   }
 
-  const body = req.body;
-  const image = await req.files.image;
+  try {
+    const body = req.body;
+    const image = await req.files.image;
 
-  if (image && image.length > 0) {
-    let imgArr = [];
-    for (let i = 0; i < image.length; i++) {
-      imgArr.push(await uploadFile(image[i]));
+    if (image && image.length > 0) {
+      let imgArr = [];
+      for (let i = 0; i < image.length; i++) {
+        imgArr.push(await uploadFile(image[i]));
+      }
+      const newAdopcion = await new Adopcion({
+        nombre: body.nombre,
+        descripcion: body.descripcion,
+        edad: parseInt(body.edad),
+        image: imgArr,
+        especie: body.especie,
+        raza: body.raza,
+      }).save();
+
+      return res.status(200).json({ newAdopcion });
     }
-    const newAdopcion = await new Adopcion({
-      nombre: body.nombre,
-      descripcion: body.descripcion,
-      edad: parseInt(body.edad),
-      image: imgArr,
-      especie: body.especie,
-      raza: body.raza,
-    }).save();
 
-    return res.status(200).json({ newAdopcion });
+    res.status(400).json({ message: "Se debe enviar una imagen" });
+  } catch (error) {
+    next(error);
   }
-
-  res.status(400).json({ message: "Se debe enviar una imagen" });
 };
